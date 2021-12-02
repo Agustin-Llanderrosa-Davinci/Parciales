c963267c348945bdfd154d3b7cce36fbe6355418
Agustin-Llanderrosa-Davinci <89217835+Agustin-Llanderrosa-Davinci@users.noreply.github.com>
2021-12-02 19:17:13 -0300

    Add files via upload

---
+++ Trabajo-Practico/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        CuentaBancaria CuentaBancaria = new CuentaBancaria("Agustin Llanderrosa","42686032");
+
+        CuentaBancaria.mostrarMenu();
+    }
+    }
+
